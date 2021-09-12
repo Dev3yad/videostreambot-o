70d8b25f5ef2368313ba15158ffcae2d934ffae2
Dev3yad <88689277+Dev3yad@users.noreply.github.com>
2021-09-12 22:22:24 +0200

    Add files via upload

--- Bot/video_stream.py
+++ Bot/video_stream.py
@@ -9,7 +9,7 @@ API_ID = os.environ.get("API_ID",12345)
 API_HASH = os.environ.get("API_HASH","")
 SESSION_NAME = os.environ.get("SESSION_NAME","")
 CHAT = os.environ.get("CHAT","")
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+ADMIN = int(os.environ.get("ADMIN", 944353237))
 
 app = Client(SESSION_NAME, API_ID, API_HASH)
 
@@ -18,7 +18,7 @@ VIDEO_CALL = {}
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["start"]))
 async def start(client, m: Message):
-	await m.reply("Hello Start Stream Video Using Command /play(reply_to_message) and /stop\n Check Example https://t.me/lntechnicalgroup/11740 ")
+	await m.reply("مرحبًا ابدأ تشغيل الفيديو باستخدام الأمر /play(مع الرد) او /stop لي اقافه")
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["play"]))
@@ -31,16 +31,16 @@ async def play(client, m: Message):
 				             try:
 				             	video_url = ytdl(link).besturl()
 				             except Exception as e:
-				             	await m.reply(f"**Error** -- `{e}`")
+				             	await m.reply(f"**حدث خطأ** -- `{e}`")
 				             	return
 				             try:
 				             	group_call = group_call_factory.get_group_call()
 				             	await group_call.join(CHAT)
 				             	await group_call.start_video(video_url,enable_experimental_lip_sync=True)
 				             	VIDEO_CALL[CHAT] = group_call
-				             	await m.reply("**Started  Streaming!**")
+				             	await m.reply("**بدء التشغيل!**")
 				             except Exception as e:
-				             	await m.reply(f"**Error** -- `{e}`")
+				             	await m.reply(f"**حدث خطأ** -- `{e}`")
 				             	
 					
 			else:
@@ -49,15 +49,15 @@ async def play(client, m: Message):
 			         		await group_call.join(CHAT)
 			         		await group_call.start_video(link,enable_experimental_lip_sync=True)
 			         		VIDEO_CALL[CHAT] = group_call
-			         		await m.reply("** Started Streaming!**")
+			         		await m.reply("**بدء التشغيل!**")
 			         	except Exception as e:
-			         	    	await m.reply(f"**Error** -- `{e}`")
+			         	    	await m.reply(f"**حدث خطأ** -- `{e}`")
 				             	
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["stop"]))
 async def stop (client, m: Message):
 	try:
 	       await VIDEO_CALL[CHAT].stop()
-	       await m.reply("** Stopped Streaming!**")
+	       await m.reply("**توقف البث!**")
 	except Exception as e:
-		await m.reply(f"**Error** - `{e}`")
+		await m.reply(f"**حدث خطأ** - `{e}`")
